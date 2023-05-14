ff72ac7e4b37d2ae6526fa634b2d0620be8dd60c
Achinta Sharma <23achinta@gmail.com>
2023-05-14 10:01:20 +0530

    create token based redirection singup-homepage

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -2,41 +2,59 @@ import UserInfoDisplayCard from "./components/UserInfoDisplayCard/UserInfoDispla
 import Recommendations from "./components/Recommendations/Recommendations";
 import CreatePost from "./components/CreatePost/CreatePost";
 // import Post from "./components/Post/Post";
-import { posts, userSuggestions } from "../../../mockData2";
+import { userSuggestions } from "../../../mockData2";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 // console.log("posts[0] mock data 2 in home page: ", posts[0]);
 const HomePage = () => {
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-12 gap-6 mx-6">
-      <div className="col-span-1 sm:col-span-8 h-20">
-        <CreatePost />
-        {/* <Post {...posts[0]} /> */}
-        {/* <Post {...post2} /> */}
-        {/* <Post {...post3} /> */}
-        {/* <Post {...posts[0]} /> */}
-        {/* <Post {...posts[0]} /> */}
-        {/* <CommentSection {...post3} /> */}
-      </div>
-      <div className="col-span-1 sm:col-span-4 hidden sm:block">
-        {/* Logged in user information  */}
-        <UserInfoDisplayCard />
-        {/* User recommendations */}
-        <h1
-          className="font-bold mb-4 text-xl text-gray-600"
-          style={{ textAlign: "left" }}
-        >
-          Recommendations
-        </h1>
-        <Recommendations userSuggestions={userSuggestions} />
-        {/* User's following */}
-        {/* <h1
+  // useNavigate() hook
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      // Check access token
+      const accessToken = localStorage.getItem("accessToken");
+      console.log("accessToken: ", accessToken);
+      if (!accessToken) {
+        console.log("no access token ");
+        navigate("/register");
+      } else {
+        return (
+          <div className="grid grid-cols-1 sm:grid-cols-12 gap-6 mx-6">
+            <div className="col-span-1 sm:col-span-8 h-20">
+              <CreatePost />
+              {/* <Post {...posts[0]} /> */}
+              {/* <Post {...post2} /> */}
+              {/* <Post {...post3} /> */}
+              {/* <Post {...posts[0]} /> */}
+              {/* <Post {...posts[0]} /> */}
+              {/* <CommentSection {...post3} /> */}
+            </div>
+            <div className="col-span-1 sm:col-span-4 hidden sm:block">
+              {/* Logged in user information  */}
+              <UserInfoDisplayCard />
+              {/* User recommendations */}
+              <h1
+                className="font-bold mb-4 text-xl text-gray-600"
+                style={{ textAlign: "left" }}
+              >
+                Recommendations
+              </h1>
+              <Recommendations userSuggestions={userSuggestions} />
+              {/* User's following */}
+              {/* <h1
           className="font-bold mb-4 text-xl text-gray-600"
           style={{ textAlign: "left" }}
         >
           Contacts
         </h1> */}
-      </div>
-    </div>
-  );
+            </div>
+          </div>
+        );
+      }
+    };
+    checkLoginStatus();
+  }, []);
 };
 
 export default HomePage;
--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import AppLogo from "../../assets/app-logo.png";
 
 // TODO: Refactor to an API file later.
 const AUTH_SIGNUP_API = "http://localhost:4001/merny/api/v1/auth/register";
@@ -10,6 +11,7 @@ const SignupPage = () => {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
   const handleSignup = async (event) => {
@@ -43,28 +45,26 @@ const SignupPage = () => {
 
       alert(`${message} User ${user.fullName} registered.`);
 
-      // TODO @bard Bard I want you to focus on this part
-      // redirect to home page (path: "/")
       navigate("/");
     } catch (error) {
       alert(error.response.data.message);
     }
   };
-
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
-      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-        <h1 className="font-bold text-center text-2xl mb-5">
-          Create an Account
-        </h1>
-        <form
-          onSubmit={handleSignup}
-          className="bg-white rounded-lg shadow p-10"
-        >
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center">
+      <div className="justify-center bg-white shadow-md rounded-lg p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+        <form onSubmit={handleSignup} className="flex flex-col justify-center">
+          <div className="w-full max-w-sm">
+            <img src={AppLogo} className="mb-5" />
+            <h1 className="font-bold text-center text-2xl mb-5 text-gray-600 ">
+              Create an Account
+            </h1>
+          </div>
+
           <div className="mb-5">
             <label
               htmlFor="fullName"
-              className="font-bold mb-1 text-gray-700 block"
+              className="font-bold mb-2 text-gray-700 block text-start"
             >
               Full Name
             </label>
@@ -72,29 +72,31 @@ const SignupPage = () => {
               type="text"
               name="fullName"
               value={fullName}
+              placeholder="Full name"
               onChange={(event) => setFullName(event.target.value)}
-              className="border border-gray-400 p-2 w-full rounded-lg"
+              className=" p-2 w-full px-5 py-2 rounded-lg bg-gray-100 border-0 focus:outline-none focus:ring-2 focus:ring-blue-600"
             />
           </div>
           <div className="mb-5">
             <label
               htmlFor="userName"
-              className="font-bold mb-1 text-gray-700 block"
+              className="font-bold mb-2 text-gray-700 block text-start"
             >
               Username
             </label>
             <input
               type="text"
               name="userName"
+              placeholder="Username"
               value={userName}
               onChange={(event) => setUserName(event.target.value)}
-              className="border border-gray-400 p-2 w-full rounded-lg"
+              className=" p-2 w-full px-5 py-2 rounded-lg bg-gray-100 border-0 focus:outline-none focus:ring-2 focus:ring-blue-600"
             />
           </div>
           <div className="mb-5">
             <label
               htmlFor="email"
-              className="font-bold mb-1 text-gray-700 block"
+              className="font-bold mb-2 text-gray-700 block text-start"
             >
               Email
             </label>
@@ -102,33 +104,35 @@ const SignupPage = () => {
               type="email"
               name="email"
               value={email}
+              placeholder="Email"
               onChange={(event) => setEmail(event.target.value)}
-              className="border border-gray-400 p-2 w-full rounded-lg"
+              className=" p-2 w-full px-5 py-2 rounded-lg bg-gray-100 border-0 focus:outline-none focus:ring-2 focus:ring-blue-600"
             />
           </div>
           <div className="mb-5">
             <label
               htmlFor="password"
-              className="font-bold mb-1 text-gray-700 block"
+              className="font-bold mb-2 text-gray-700 block text-start"
             >
               Password
             </label>
             <input
               type="password"
               name="password"
+              placeholder="Password"
               value={password}
               onChange={(event) => setPassword(event.target.value)}
-              className="border border-gray-400 p-2 w-full rounded-lg"
+              className=" p-2 w-full px-5 py-2 rounded-lg bg-gray-100 border-0 focus:outline-none focus:ring-2 focus:ring-blue-600"
             />
           </div>
           <div className="mb-5">
             <label
               htmlFor="gender"
-              className="block mb-2 font-bold text-gray-700"
+              className="font-bold mb-2 text-gray-700 block text-start"
             >
               Gender
             </label>
-            <div>
+            <div className="text-start">
               <label
                 htmlFor="male"
                 className="inline-flex items-center cursor-pointer"
@@ -175,10 +179,16 @@ const SignupPage = () => {
           </div>
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-3 rounded focus:outline-none focus:shadow-outline"
           >
             Sign Up
           </button>
+          <h1>
+            Already Registered?{" "}
+            <a className="text-blue-500" href="/login">
+              Login
+            </a>
+          </h1>
         </form>
       </div>
     </div>
